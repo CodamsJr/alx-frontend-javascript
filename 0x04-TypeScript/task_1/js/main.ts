047c89928709935516a54e8a2a5d0c7cb6e78d1e
CodamsJr <oguntolahammed076@gmail.com>
2025-10-20 12:02:33 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Task 1: Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -7,6 +8,7 @@ interface Teacher {
   [key: string]: any;
 }
 
+// Example for Teacher
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -16,3 +18,19 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Task 2: Directors interface extending Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example for Directors
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
